c90a4731a13ff065beb251f361c5a533681ccbe5
Denis Biondic <denis.biondic@conplement.de>
2019-01-23 19:18:40 +0100

    feat: airflow setup / init

--- CronHttpGet/initialize.sh
+++ CronHttpGet/initialize.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+rm -rf airflow_home/logs
+rm -rf airflow_home/airflow.cfg
+rm -rf airflow_home/unittests.cfg
+rm -rf airflow_home/airflow.db
+
 export SLUGIFY_USES_TEXT_UNIDECODE=yes
 export AIRFLOW_HOME=`pwd`/airflow_home
+export AIRFLOW__CORE__LOAD_EXAMPLES=False
 
 pip install -r requirements.txt
+
+airflow initdb
